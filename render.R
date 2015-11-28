509492e7ab7d3c353b2c6773d10032b7de38918d
kazutan <kazu@kz-md.net>
2015-11-28 15:09:57 +0900

    update

--- render.R
+++ render.R
@@ -4,3 +4,30 @@ rmarkdown::render("data_handling.Rmd", output_format = "revealjs::revealjs_prese
 rmarkdown::render("data_handling.Rmd", output_format = "html_document", output_file = "data_handling_d.html")
 rmarkdown::render("ggplot.Rmd", output_format = "revealjs::revealjs_presentation", output_file = "ggplot2_s.html")
 rmarkdown::render("ggplot.Rmd", output_format = "html_document", output_file = "ggplot2_d.html")
+rmarkdown::render("leaflet_lite.Rmd", output_format = "revealjs::revealjs_presentation", output_file = "leaflet_lite_s.html")
+
+
+
+
+# for leaflet in reveal.js
+knit_print.htmlwidget <- function (widget, ..., options = NULL) 
+{
+	#  WIDTH  <- "100%"
+	#  HEIGHT <- 500
+	
+	file <- basename(tempfile(fileext = ".html"))
+	selfcontained <- if(is.null(rmarkdown::metadata$self_contained)) TRUE else rmarkdown::metadata$self_contained
+	
+	htmlwidgets::saveWidget(widget, file = file, selfcontained = selfcontained)
+	content <- if (selfcontained) {
+		on.exit(unlink(file), add = TRUE)
+		list(srcdoc = paste(readLines(file), collapse = "\n")) 
+	}
+	else {
+		list(src = file)
+	}
+	
+	x <- htmltools::tag("iframe", content)
+	
+	knitr::knit_print(x, options = options, ...)
+}
